2b2615c0e27091e98c058b14f7be95c802137414
Alec Szczechowicz <65929878+xBruzx@users.noreply.github.com>
2025-02-21 12:05:13 -0600

    working
    yay :D

--- celcius.sh
+++ celcius.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #prompt the user for input temp
-echo "Please enter in the temperataure outside: "
+echo "Please enter in the temperature outside: "
 read tempF
 
-tempC = ((tempF - 32) * 5 / 9)
+tempC=$(( (tempF - 32) * 5 / 9))
 
 echo "Your temp in C is $tempC C"
