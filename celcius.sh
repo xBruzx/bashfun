ec534dad4b29117c77a26c40685b4c02937e2836
Alec Szczechowicz <65929878+xBruzx@users.noreply.github.com>
2025-02-21 10:47:31 -0600

    2 realy fun files I made and only one works
    EPIC

---
+++ celcius.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#prompt the user for input temp
+echo "Please enter in the temperataure outside: "
+read tempF
+
+tempC = ((tempF - 32) * 5 / 9)
+
+echo "Your temp in C is $tempC C"
---
+++ latin_pig_name.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Prompting the user for their name so that way we can make it into a pig name.
+echo "Please Enter you name: "
+read name
+
+#Echoing the name back as a pig name
+
+echo "Your latin pig name is $name-ay"
